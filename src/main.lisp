ee5c31ffe335e063c884689d00d9d0f5b4f2ed75
selwyn <selwyn.simsek@cantab.net>
2024-11-04 11:57:37 +0000

    put comments inside docstrings

--- src/main.lisp
+++ src/main.lisp
@@ -33,166 +33,167 @@
 
 ;;; CFFI bindings
 
-;; Return the name and the version of the incremental SAT solving library.
+(cffi:defcfun (%ipasir-signature "ipasir_signature") (:pointer :char)
+  "Return the name and the version of the incremental SAT solving library.
 
-;; IPASIR_API const char * ipasir_signature ();
+   IPASIR_API const char * ipasir_signature ();")
 
-(cffi:defcfun (%ipasir-signature "ipasir_signature") (:pointer :char))
+(cffi:defcfun (%ipasir-init "ipasir_init") :pointer
+  "Construct a new solver and return a pointer to it.
 
-;; Construct a new solver and return a pointer to it.
-;; Use the returned pointer as the first parameter in each of the following functions.
+   Required state: N/A
+   State after: INPUT
 
-;; Required state: N/A
-;; State after: INPUT
+   IPASIR_API void * ipasir_init ();")
 
-;; IPASIR_API void * ipasir_init ();
+(cffi:defcfun (%ipasir-release "ipasir_release") :void
+  "Release the solver, i.e., all its resources and allocated memory (destructor).
+   The solver pointer cannot be used for any purposes after this call.
 
-(cffi:defcfun (%ipasir-init "ipasir_init") :pointer)
+   Required state: INPUT or SAT or UNSAT
+   State after: undefined
 
-;; Release the solver, i.e., all its resources and allocated memory (destructor).
-;; The solver pointer cannot be used for any purposes after this call.
-
-;; Required state: INPUT or SAT or UNSAT
-;; State after: undefined
-
-;; IPASIR_API void ipasir_release (void * solver);
-
-(cffi:defcfun (%ipasir-release "ipasir_release") :void (solver :pointer))
-
-;; Add the given literal into the currently added clause
-;; or finalize the clause with a 0.  Clauses added this way
-;; cannot be removed. The addition of removable clauses
-;; can be simulated using activation literals and assumptions.
-
-;; Required state: INPUT or SAT or UNSAT
-;; State after: INPUT
-
-;; Literals are encoded as (non-zero) integers as in the
-;; DIMACS formats.  They have to be smaller or equal to
-;; INT32_MAX and strictly larger than INT32_MIN (to avoid
-;; negation overflow).  This applies to all the literal
-;; arguments in API functions.
-
-;; IPASIR_API void ipasir_add (void * solver, int32_t lit_or_zero);
-
-(cffi:defcfun (%ipasir-add "ipasir_add") :void (solver :pointer) (lit-or-zero :int32))
-
-;; Add an assumption for the next SAT search (the next call
-;; of ipasir_solve). After calling ipasir_solve all the
-;; previously added assumptions are cleared.
-
-;; Required state: INPUT or SAT or UNSAT
-;; State after: INPUT
-
-;; IPASIR_API void ipasir_assume (void * solver, int32_t lit);
-
-(cffi:defcfun (%ipasir-assume "ipasir_assume") :void (solver :pointer) (lit :int32))
-
-;; Solve the formula with specified clauses under the specified
-;; assumptions.  If the formula is satisfiable the function returns 10
-;; and the state of the solver is changed to SAT.  If the formula is
-;; unsatisfiable the function returns 20 and the state of the solver is
-;; changed to UNSAT.  If the search is interrupted (see
-;; ipasir_set_terminate) the function returns 0 and the state of the
-;; solver is changed to INPUT.  This function can be called in any
-;; defined state of the solver.  Note that the state of the solver
-;; _during_ execution of 'ipasir_solve' is undefined.
-
-;; Required state: INPUT or SAT or UNSAT
-;; State after: INPUT or SAT or UNSAT
-
-;; IPASIR_API int ipasir_solve (void * solver);
-
-(cffi:defcfun (%ipasir-solve "ipasir_solve") :int (solver :pointer))
-
-;; Get the truth value of the given literal in the found satisfying
-;; assignment.  Return 'lit' if True, '-lit' if False; 'ipasir_val(lit)'
-;; may return '0' if the found assignment is satisfying for both
-;; valuations of lit.  Each solution that agrees with all non-zero
-;; values of ipasir_val() is a model of the formula.
-
-;; This function can only be used if ipasir_solve has returned 10
-;; and no 'ipasir_add' nor 'ipasir_assume' has been called
-;; since then, i.e., the state of the solver is SAT.
-
-;; Required state: SAT
-;; State after: SAT
-
-;; IPASIR_API int32_t ipasir_val (void * solver, int32_t lit);
-
-(cffi:defcfun (%ipasir-val "ipasir_val") :int32 (solver :pointer) (lit :int32))
+   IPASIR_API void ipasir_release (void * solver);"
+  (solver :pointer))
 
 
-;; Check if the given assumption literal was used to prove the
-;; unsatisfiability of the formula under the assumptions
-;; used for the last SAT search. Return 1 if so, 0 otherwise.
+(cffi:defcfun (%ipasir-add "ipasir_add") :void
+  "Add the given literal into the currently added clause
+   or finalize the clause with a 0.  Clauses added this way
+   cannot be removed. The addition of removable clauses
+   can be simulated using activation literals and assumptions.
 
-;; The formula remains unsatisfiable even just under assumption literals
-;; for which ipasir_failed() returns 1.  Note that for literals 'lit'
-;; which are not assumption literals, the behavior of
-;; 'ipasir_failed(lit)' is not specified.
+   Required state: INPUT or SAT or UNSAT
+   State after: INPUT
 
-;; This function can only be used if ipasir_solve has returned 20 and
-;; no ipasir_add or ipasir_assume has been called since then, i.e.,
-;; the state of the solver is UNSAT.
+   Literals are encoded as (non-zero) integers as in the
+   DIMACS formats.  They have to be smaller or equal to
+   INT32_MAX and strictly larger than INT32_MIN (to avoid
+   negation overflow).  This applies to all the literal
+   arguments in API functions.
 
-;; Required state: UNSAT
-;; State after: UNSAT
+   IPASIR_API void ipasir_add (void * solver, int32_t lit_or_zero);"
+  (solver :pointer) (lit-or-zero :int32))
 
-;; IPASIR_API int ipasir_failed (void * solver, int32_t lit);
 
-(cffi:defcfun (%ipasir-failed "ipasir_failed") :int (solver :pointer) (lit :int32))
+(cffi:defcfun (%ipasir-assume "ipasir_assume") :void
+  "Add an assumption for the next SAT search (the next call
+   of ipasir_solve). After calling ipasir_solve all the
+   previously added assumptions are cleared.
 
-;; Set a callback function used to indicate a termination requirement to
-;; the solver.  The solver will periodically call this function and
-;; check its return value during the search.  The ipasir_set_terminate
-;; function can be called in any state of the solver, the state remains
-;; unchanged after the call.  The callback function is of the form
-;; "int terminate(void * data)"
-;;   - it returns a non-zero value if the solver should terminate.
-;;   - the solver calls the callback function with the parameter "data"
-;;     having the value passed in the ipasir_set_terminate function
-;;     (2nd parameter).
+   Required state: INPUT or SAT or UNSAT
+   State after: INPUT
 
-;; Required state: INPUT or SAT or UNSAT
-;; State after: INPUT or SAT or UNSAT
+   IPASIR_API void ipasir_assume (void * solver, int32_t lit);"
+  (solver :pointer) (lit :int32))
 
-;; IPASIR_API void ipasir_set_terminate (void * solver, void * data, int (*terminate)(void * data));
+(cffi:defcfun (%ipasir-solve "ipasir_solve") :int
+  "Solve the formula with specified clauses under the specified
+   assumptions.  If the formula is satisfiable the function returns 10
+   and the state of the solver is changed to SAT.  If the formula is
+   unsatisfiable the function returns 20 and the state of the solver is
+   changed to UNSAT.  If the search is interrupted (see
+   ipasir_set_terminate) the function returns 0 and the state of the
+   solver is changed to INPUT.  This function can be called in any
+   defined state of the solver.  Note that the state of the solver
+   _during_ execution of 'ipasir_solve' is undefined.
 
-(cffi:defcfun (%ipasir-set-terminate "ipasir_set_terminate") :void (solver :pointer) (data :pointer) (terminate :pointer))
+   Required state: INPUT or SAT or UNSAT
+   State after: INPUT or SAT or UNSAT
+
+   IPASIR_API int ipasir_solve (void * solver);"
+  (solver :pointer))
+
+
+(cffi:defcfun (%ipasir-val "ipasir_val") :int32
+  "Get the truth value of the given literal in the found satisfying
+   assignment.  Return 'lit' if True, '-lit' if False; 'ipasir_val(lit)'
+   may return '0' if the found assignment is satisfying for both
+   valuations of lit.  Each solution that agrees with all non-zero
+   values of ipasir_val() is a model of the formula.
+
+   This function can only be used if ipasir_solve has returned 10
+   and no 'ipasir_add' nor 'ipasir_assume' has been called
+   since then, i.e., the state of the solver is SAT.
+
+   Required state: SAT
+   State after: SAT
+
+   IPASIR_API int32_t ipasir_val (void * solver, int32_t lit);"
+  (solver :pointer) (lit :int32))
+
+
+
+(cffi:defcfun (%ipasir-failed "ipasir_failed") :int
+  "Check if the given assumption literal was used to prove the
+   unsatisfiability of the formula under the assumptions
+   used for the last SAT search. Return 1 if so, 0 otherwise.
+
+   The formula remains unsatisfiable even just under assumption literals
+   for which ipasir_failed() returns 1.  Note that for literals 'lit'
+   which are not assumption literals, the behavior of
+   'ipasir_failed(lit)' is not specified.
+
+   This function can only be used if ipasir_solve has returned 20 and
+   no ipasir_add or ipasir_assume has been called since then, i.e.,
+   the state of the solver is UNSAT.
+
+   Required state: UNSAT
+   State after: UNSAT
+
+   IPASIR_API int ipasir_failed (void * solver, int32_t lit);"
+  (solver :pointer) (lit :int32))
+
+
+(cffi:defcfun (%ipasir-set-terminate "ipasir_set_terminate") :void
+  "Set a callback function used to indicate a termination requirement to
+   the solver.  The solver will periodically call this function and
+   check its return value during the search.  The ipasir_set_terminate
+   function can be called in any state of the solver, the state remains
+   unchanged after the call.  The callback function is of the form
+   int terminate(void * data)
+   - it returns a non-zero value if the solver should terminate.
+   - the solver calls the callback function with the parameter data
+    having the value passed in the ipasir_set_terminate function
+    (2nd parameter).
+
+   Required state: INPUT or SAT or UNSAT
+   State after: INPUT or SAT or UNSAT
+
+   IPASIR_API void ipasir_set_terminate (void * solver, void * data, int (*terminate)(void * data));"
+  (solver :pointer) (data :pointer) (terminate :pointer))
 
 (cffi:defcallback example-terminate :int ((data :pointer))
   (declare (ignore data))
-  ;(format *error-output* "in example-terminate: returning 0~%")
   0)
 
-;; Set a callback function used to extract learned clauses up to a given
-;; length from the solver.  The solver will call this function for each
-;; learned clause that satisfies the maximum length (literal count)
-;; condition.  The ipasir_set_learn function can be called in any state
-;; of the solver, the state remains unchanged after the call.  The
-;; callback function is of the form
-;; "void learn(void * data, int * clause)"
-;;   - the solver calls the callback function with the parameter "data"
-;;     having the value passed in the ipasir_set_learn function
-;;     (2nd parameter).
-;;   - the argument "clause" is a pointer to a null terminated integer
-;;     array containing the learned clause. the solver can change the
-;;     data at the memory location that "clause" points to after the
-;;     function call.
-;;   - the solver calls the callback function from the same thread
-;;     in which ipasir_solve has been called.
+(cffi:defcfun (%ipasir-set-learn "ipasir_set_learn") :void
+  "Set a callback function used to extract learned clauses up to a given
+   length from the solver.  The solver will call this function for each
+   learned clause that satisfies the maximum length (literal count)
+   condition.  The ipasir_set_learn function can be called in any state
+   of the solver, the state remains unchanged after the call.  The
+   callback function is of the form
+   void learn(void * data, int * clause)
+     - the solver calls the callback function with the parameter data
+       having the value passed in the ipasir_set_learn function
+       (2nd parameter).
+     - the argument clause is a pointer to a null terminated integer
+       array containing the learned clause. the solver can change the
+       data at the memory location that clause points to after the
+       function call.
+     - the solver calls the callback function from the same thread
+       in which ipasir_solve has been called.
 
-;; Subsequent calls to ipasir_set_learn override the previously
-;; set callback function.  Setting the callback function to NULL
-;; with any max_length argument disables the callback.
+   Subsequent calls to ipasir_set_learn override the previously
+   set callback function.  Setting the callback function to NULL
+   with any max_length argument disables the callback.
 
-;; Required state: INPUT or SAT or UNSAT
-;; State after: INPUT or SAT or UNSAT
+   Required state: INPUT or SAT or UNSAT
+   State after: INPUT or SAT or UNSAT
 
-;; IPASIR_API void ipasir_set_learn (void * solver, void * data, int max_length, void (*learn)(void * data, int32_t * clause));
-
-(cffi:defcfun (%ipasir-set-learn "ipasir_set_learn") :void (solver :pointer) (data :pointer) (max-length :int) (learn :pointer))
+   IPASIR_API void ipasir_set_learn (void * solver, void * data, int max_length, void (*learn)(void * data, int32_t * clause));"
+  (solver :pointer) (data :pointer) (max-length :int) (learn :pointer))
 
 
 (cffi:defcallback example-learn :void ((data :pointer) (clause (:pointer :int)))
@@ -202,35 +203,30 @@
                     collect (cffi:mem-ref clause :int i))))
     (format *error-output* "in example learn: extracting learnt clause~a~%" data)))
 
-;; void ipasir_set_seed (void * solver, int seed);
 
 (cffi:defcfun (%ipasir-set-seed "ipasir_set_seed") :void (solver :pointer) (seed :int))
 
-;; Addition to the interface: enables to create a non-incremental solver which can then be branched
-;; via mallob_ipasir_branched_solve ().
+(cffi:defcfun (%mallob-ipasir-init "mallob_ipasir_init") :pointer
+  "Addition to the interface: enables to create a non-incremental solver which can then be branched
+via mallob_ipasir_branched_solve ().
 
-;; void* mallob_ipasir_init (bool incremental);
+   void* mallob_ipasir_init (bool incremental);"
+  (incremental :boolean))
 
-(cffi:defcfun (%mallob-ipasir-init "mallob_ipasir_init") :pointer (incremental :boolean))
+(cffi:defcfun (%mallob-ipasir-branched-solve "mallob_ipasir_branched_solve") :void
+  "Addition to the interface: branch off a child solver on the current formulae / assumptions,
+   call the provided callback as soon as solving is done. Clears assumptions in the parent solver.
 
-;; Addition to the interface: branch off a child solver on the current formulae / assumptions,
-;; call the provided callback as soon as solving is done. Clears assumptions in the parent solver.
-
-;; void mallob_ipasir_branched_solve (void * solver, void * data, int (*terminate)(void * data), void (*callback_done)(int result, void* child_solver, void* data));
-
-(cffi:defcfun (%mallob-ipasir-branched-solve "mallob_ipasir_branched_solve")
-    :void (solver :pointer) (data :pointer) (terminate :pointer) (callback-done :pointer))
+   void mallob_ipasir_branched_solve (void * solver, void * data, int (*terminate)(void * data), void (*callback_done)(int result, void* child_solver, void* data));"
+  (solver :pointer) (data :pointer) (terminate :pointer) (callback-done :pointer))
 
 (cffi:defcallback example-callback-done :void ((result :int) (child-solver :pointer) (data :pointer))
   (format *error-output* "in example-callback-done: ~a ~a ~a~%" result child-solver data))
 
-
-;; Addition to the interface: already begin to introduce the next job to Mallob (via ipasir_solve, not branched_solve!).
-;; Illegal if the formula transfer mode is not NAMED_PIPE.
-
-;; void mallob_ipasir_presubmit (void * solver);
-
-(cffi:defcfun (%mallob-ipasir-presubmit "mallob_ipasir_presubmit") :void (solver :pointer))
+(cffi:defcfun (%mallob-ipasir-presubmit "mallob_ipasir_presubmit") :void
+  "Addition to the interface: already begin to introduce the next job to Mallob (via ipasir_solve, not branched_solve!).
+   Illegal if the formula transfer mode is not NAMED_PIPE."
+  (solver :pointer))
 
 ;;; High level API
 
@@ -293,7 +289,10 @@
   ((state :initarg :state :accessor state)
    (solver :initarg :solver :accessor solver)))
 
+(defun check-sat (&optional (solver *sat-solver*)) (eq :sat (solve solver)))
+
 (defun solve (&optional (solver *sat-solver*))
+  "Tries to solve the solver. Returns one of :sat, :unsat or :input."
   (restart-case
       (bind (((:accessors solver-pointer solver-state added-literals) solver))
         (unless (member solver-state (list :sat :unsat :input) :test 'eq)
@@ -388,7 +387,9 @@
           symbol-table
           solver-state
           release-solver
-          add-literal solve
+          add-literal
+          solve
+          check-sat
           add-assumption
           clause-not-terminated
           model
