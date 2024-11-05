9c9c03a83738552820d1d796d3a8573cfedc4efb
selwyn <selwyn.simsek@cantab.net>
2024-11-05 10:52:00 +0000

    more convenient testing

---
+++ tests/test.sh
@@ -0,0 +1,2 @@
+sbcl --eval '(ql:quickload :sat-lisp)' --eval "(sat-lisp::ensure-ipasir-loaded (quote sat-lisp:$1 ))" --eval '(sat-lisp:with-sat-solver (mapcar (quote sat-lisp:add-literal) (list 5 0 2 4 0 1 0))   (when (sat-lisp:check-sat) (print (sat-lisp:model)))(mapcar (quote sat-lisp:add-literal) (list 3 7 -8 0 10 0)) (when (sat-lisp:check-sat) (print (sat-lisp:model)))(mapcar (quote sat-lisp:add-literal) (list  -2 0  49 -50 0)) (when (sat-lisp:check-sat) (print (sat-lisp:model))))' --non-interactive 
+
